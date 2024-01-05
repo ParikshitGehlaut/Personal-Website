06893a8af75bfe2cc3ab5bab06345553bcd66831
ParikshitGehlaut <139605784+ParikshitGehlaut@users.noreply.github.com>
2024-01-05 18:52:13 +0530

    Update layout.js
    Google verfication

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,6 +16,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="fzo1OSEJ39iT3DzacFMoPBj6BCVK0w_6kV_ts7EJlv4"
+        />
+      </head>
       <body className={inter.className}>
         <div className="container">
           <header>
