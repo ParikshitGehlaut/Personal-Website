2eb9a33f7cfda1a6d796b7a7803e02dcb37bf586
ParikshitGehlaut <139605784+ParikshitGehlaut@users.noreply.github.com>
2024-02-05 21:02:31 +0530

    remove kinde auth

--- src/app/components/navbar/Navbar.jsx
+++ src/app/components/navbar/Navbar.jsx
@@ -2,17 +2,8 @@ import React from "react";
 import styles from "./navbar.module.css";
 import Link from "next/link";
 import ResponsiveNav from "../responsiveNav/ResponsiveNav";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import {
-  RegisterLink,
-  LoginLink,
-  LogoutLink,
-} from "@kinde-oss/kinde-auth-nextjs/components";
 
 const Navbar = async () => {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
-  // console.log(user);
   return (
     <div className={styles.container}>
       <div className={styles.logo}>Parikshit's Portfolio</div>
@@ -29,26 +20,6 @@ const Navbar = async () => {
         <Link href="/contact" className={styles.link}>
           Contact Me
         </Link>
-        {/* <Link href="https://drive.google.com/file/d/1eq7AqEaFHR-ComeGiRW-u7Nf68CcUllC/view?usp=sharing">
-          <button className={styles.btn}>Resume</button>
-        </Link> */}
-        {!user ? (
-          <>
-            <button className={styles.btn}>
-              <LoginLink>Sign in</LoginLink>
-            </button>
-            <button className={styles.btn}>
-              <RegisterLink>Sign up</RegisterLink>
-            </button>
-          </>
-        ) : (
-          <>
-            {/* <span>Hello,{user.given_name}</span> */}
-            <button className={styles.btn}>
-              <LogoutLink>Log out</LogoutLink>
-            </button>
-          </>
-        )}
       </div>
       <ResponsiveNav />
     </div>
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -6,7 +6,7 @@ import styles from "./contact.module.css";
 export const metadata = {
   title: "Contact",
   description:
-    "Welcome to Parikshit Gehlaut's Portfolio.I am beginner Full Stack developer.With a passion for creating stunning, functional websites, I'm here to bring your online vision to life. Contact me today to discuss your next project",
+    "Welcome to Parikshit Gehlaut's Portfolio. I am beginner Full Stack developer.With a passion for creating stunning, functional websites, I'm here to bring your online vision to life. Contact me today to discuss your next project",
 };
 
 const Contact = () => {
@@ -25,22 +25,32 @@ const Contact = () => {
         <div className={styles.icons}>
           <Link
             href="https://www.instagram.com/parikshitgehlaut/"
+            title="Instagram"
             target="_blank"
           >
             {" "}
             <Image src="/instagram.webp" alt="" height={50} width={50} />
           </Link>
-          <Link href="https://medium.com/@parikshitgehlaut" target="_blank">
+          <Link
+            href="https://medium.com/@parikshitgehlaut"
+            target="_blank"
+            title="Medium"
+          >
             {" "}
             <Image src="/medium.webp" alt="" height={50} width={50} />
           </Link>
-          <Link href="https://twitter.com/I_AM_PARIKSHIT" target="_blank">
+          <Link
+            href="https://twitter.com/I_AM_PARIKSHIT"
+            target="_blank"
+            title="Twitter"
+          >
             {" "}
             <Image src="/Twitter.webp" alt="" height={50} width={50} />
           </Link>
           <Link
             href="https://www.linkedin.com/in/parikshitgehlaut/"
             target="_blank"
+            title="LinkedIn"
           >
             {" "}
             <Image src="/linkedin.webp" alt="" height={50} width={50} />
